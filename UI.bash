e8d06f68fffb79184a5597314b06cd5d33b0c9a4
SlightlyRoasted <113511213+SlightlyRoasted@users.noreply.github.com>
2022-12-20 17:38:47 +0100

    Update UI.bash

--- UI.bash
+++ UI.bash
@@ -26,7 +26,7 @@ UI() {
 				2)
 					echo "test2";;
 				3)
-					echo "test3";;
+					groupManager;;
 				4)
 					echo "test4";;
 				5)
@@ -37,6 +37,149 @@ UI() {
 			esac
 		done
 	fi
+}
+groupManager() {
+	managerVal=1
+	while [ "$managerVal" -eq "1" ]; do
+		clear
+		echo -e "\nGroup Manager Menu!"
+		echo "***************"
+		echo "1. Create group"
+		echo "2. List User Groups"
+		echo "3. List Users In Chosen Group"
+		echo "4. Add User To Group"
+		echo "5. Remove User From Group"
+		echo "6. Remove A User Created Group"
+		echo "7. Back To Main Menu"
+		read -n 1 -p "Enter your option here: " -s USERCHOICE
+		clear
+
+		case $USERCHOICE in
+			1)
+				groupName=""
+				read -p "Enter desired group name: " groupName
+				groupadd $groupName
+				if [ $? -eq 0 ]; then
+					clear
+					echo -e "\nGroup "$groupName" created!\nPress any button to continue"
+					read -n 1
+				else	
+					clear
+					echo -e "\nGroup "$groupName" already exists! No new group was created!"
+					echo -e "\nPress any button to continue"
+					read -n 1
+				fi
+				;;
+			2)
+				getent group | egrep ":[0-9][0-9][0-9][0-9]:" | cut -d ':' -f1 
+				echo -e "\nPress any button to go back to main menu"
+				read -n 1
+				;;
+			3)
+				groupName=""
+				read -p "Enter which groups members you wish to display: " groupName
+				getent group $groupName
+				if [ $? -eq 0 ]; then
+					grep $groupName /etc/passwd
+					if [ $? -eq 0 ]; then
+						clear
+						echo -e "\nGroup members of "$groupName": "
+						echo -e -n ""$groupName""
+						getent group $groupName | cut -d ':' -f4
+						echo -e "\nPress any button to continue"
+						read -n 1
+					else	
+						clear
+						echo -e "\nGroup members of "$groupName": "
+						getent group $groupName | cut -d ':' -f4
+						echo -e "\nPress any button to continue"
+						read -n 1
+					fi
+				else	
+					clear
+					echo -e "\nThere is no group with that name"
+					echo -e "\nPress any button to continue"
+					read -n 1
+				fi
+				;;
+			4)
+				groupName=""
+				userName=""
+				read -p "Enter the name of the user you wish to add to a group : " userName
+				grep $userName /etc/passwd
+				if [ $? -eq 0 ]; then
+					clear
+					read -p "Enter which group you want the member to join: " groupName
+					getent group $groupName
+					if [ $? -eq 0 ]; then
+						usermod -a -G $groupName $userName
+						clear
+						echo -e "\n"$userName" has now been added to "$groupName""
+						echo -e "\nPress any button to continue"
+						read -n 1
+					else
+						clear
+						echo -e "\nChosen group does not exist!"
+						echo -e "\nPress any button to continue"
+						read -n 1
+					fi
+				else
+					clear
+						echo -e "\nChosen user does not exist!"
+						echo -e "\nPress any button to continue"
+						read -n 1
+				fi
+				;;
+			5)
+				groupName=""
+				userName=""
+				read -p "Enter the name of the user you wish to remove from a group : " userName
+				grep $userName /etc/passwd
+				if [ $? -eq 0 ]; then
+					clear
+					read -p "Enter which group you want the member removed from: " groupName
+					getent group $groupName
+					if [ $? -eq 0 ]; then
+						gpasswd --delete $userName $groupName
+						clear
+						echo -e "\n"$userName" has now been removed from "$groupName""
+						echo -e "\nPress any button to continue"
+						read -n 1
+					else
+						clear
+						echo -e "\nChosen group does not exist!"
+						echo -e "\nPress any button to continue"
+						read -n 1
+					fi
+				else
+					clear
+						echo -e "\nChosen user does not exist!"
+						echo -e "\nPress any button to continue"
+						read -n 1
+				fi
+
+				;;
+			6)
+				groupName=""
+				read -p "Enter the name of the group you wish to delete: " groupName
+				groupdel $groupName
+				if [ $? -eq 0 ]; then
+					clear
+					echo -e "\nGroup "$groupName" deleted!\nPress any button to continue"
+					read -n 1
+				else	
+					clear
+					echo -e "\nGroup "$groupName" doesnt exist! No group was deleted!"
+					echo -e "\nPress any button to continue"
+					read -n 1
+				fi
+				;;
+			7)
+				managerVal=0;;
+			*)
+				echo "Invalid input";;
+		esac
+	done
 }
 
 networkInfo() {
