3b86cc75b87fd2b82b89f4c2b3f9f99069b06a91
TedBoman <113337597+TedBoman@users.noreply.github.com>
2022-12-20 17:43:22 +0100

    Update UI.bash

--- UI.bash
+++ UI.bash
@@ -24,7 +24,7 @@ UI() {
 				1)
 					networkInfo;;
 				2)
-					echo "test2";;
+					userMenu;;
 				3)
 					groupManager;;
 				4)
@@ -180,6 +180,227 @@ groupManager() {
 				echo "Invalid input";;
 		esac
 	done
+}
+
+
+userMenu() {
+	USERMENUCHOICE=1
+	while [ "$USERMENUCHOICE" -eq "1" ]; do
+		clear
+		echo -e "\nUserMenu"
+		echo "************************************"
+		echo -e "1. Add user"
+		echo -e "2. Delete user"
+		echo -e "3. List all users"
+		echo -e "4. View all attributes for a user"
+		echo -e "5. Change user attributes"
+		echo -e "6. Return"
+		read -n 1 -p "Enter your option here: " -s USERCHOICE
+		clear
+
+		case $USERCHOICE in
+				1)
+					USERNAME=""
+					echo "************************************"
+					echo "Follow the steps below to add a user"
+					echo "************************************"
+					read -p "Enter username (MAX 32): " USERNAME
+					id $USERNAME &> /dev/null
+					if [ $? = 0 ]; then
+						echo "This user already exists!"
+					else
+						adduser $USERNAME
+						echo "The user $USERNAME has been added!"
+					fi
+					echo -e "\nPress any button to continue"
+					read -n 1
+					;;
+				2)
+					USERNAME=""
+					echo "************************************"
+					echo "Follow these steps to delete a user"
+					echo "************************************"
+					read -p "Enter the user: " USERNAME
+					id $USERNAME &> /dev/null
+					if [ $? = 1 ]; then
+						echo "This user does not exist!"
+					else					
+						userdel $USERNAME
+						echo "This user $USERNAME has been deleted!"
+					fi
+					echo -e "\nPress any button to continue"
+					read -n 1
+					;;
+				3)
+					echo "**************************************"
+					echo "Displaying all the users on the system"
+					echo "**************************************"
+					grep [0-9] /etc/passwd | grep -v -e nologin | awk -F: '{print $3, $1}' | column -t -N "USERID,USERNAME" | awk 'NR == 1; NR > 1 {print $0 | "sort -n"}'
+					echo -e "\nPress any button to continue"
+					read -n 1
+					;;
+				4)
+					USERNAME=""
+					echo "************************************"					
+					echo "Show attributes for specific user"
+					echo "************************************"
+					read -p "Enter username to view attributes: " USERNAME
+					id $USERNAME &> /dev/null
+					if [ $? = 1 ]; then
+						echo "This user does not exist!"
+					else
+						echo "Attributes for user: $USERNAME"
+						echo -e "User:\t\t$USERNAME"
+						echo -e "User ID:\t\c"
+						id $USERNAME | awk '{print $1}' | sed 's/[^0-9]*//g'
+						echo -e "Group ID: \t\c"
+						id $USERNAME | awk '{print $2}' | sed 's/[^0-9]*//g'
+						echo -e "Comment:\t\c"
+						getent passwd $USERNAME | sed 's/,/ /g' | awk -F: '{print $5}'
+						echo -e "Directory:\t\c"
+						getent passwd $USERNAME | sed 's/,/ /g' | awk -F: '{print $6}'
+						echo -e "Shell\t\t\c"
+						getent passwd $USERNAME | sed 's/,/ /g' | awk -F: '{print $7}'
+					fi	
+					echo -e "\nPress any button to continue"
+					read -n 1	
+					;;
+				5)
+					ATTRIBUTES_FLAG=1
+					
+					while [ "$ATTRIBUTES_FLAG" -eq "1" ]; do
+						clear
+						echo "************************************"
+						echo "Change user attributes"
+						echo "************************************"
+						echo -e "\n1. Change username"
+						echo "2. Change userID"
+						echo "3. Change home directory"
+						echo "4. Change shell path"
+						echo "5. Change user comment"
+						echo "6. Change user password"
+						echo "7. Return"
+						read -n 1 -p "Enter your option here: " USERCHOICE
+						clear
+
+						case $USERCHOICE in
+							1)
+						
+								USERNAME=""
+								echo "************************************"					
+								echo "Change username"
+								echo "************************************"								
+								read -p "Enter old username" USERNAME
+								id $USERNAME &> /dev/null
+								if [ $? = 1 ]; then
+									echo "This user does not exist!"									
+								else
+									NEWUSERNAME=""
+									read -p "Enter new username: " NEWUSERNAME
+									usermod -l $NEWUSERNAME $OLDUSERNAME
+								fi
+								;;
+							2)
+								NEWID=0
+								USERNAME=""
+								echo "************************************"					
+								echo "Change userID"
+								echo "************************************"								
+
+								read -p "Enter user: " USERNAME
+								id $USERNAME &> /dev/null
+								if [ $? = 1 ]; then
+									echo "This user does not exist!"
+								else	
+									NEWUSERNAME=""
+									read -p "Enter new userID (MUST BE UNIQUE): " NEWID
+									usermod -u $NEWID $USERNAME
+								fi
+								;;
+							3)
+								USERNAME=""
+								NEWHOME=""
+								echo "************************************"					
+								echo "Change home directory"
+								echo "************************************"	
+								read -p "Enter user: " USERNAME
+								id $USERNAME &> /dev/null
+								if [ $? = 1 ]; then
+									echo "This user does not exist!"
+								else
+									read -p "Enter the new home directory: " NEWHOME
+									usermod -d $NEWHOME $USERNAME
+								fi
+								;;
+							4)
+								USERNAME=""
+								NEWSHELL=""
+								echo "************************************"					
+								echo "Change shell path"
+								echo "************************************"								
+
+								read -p "Enter username" USERNAME
+								id $USERNAME &> /dev/null
+								if [ $? = 1 ]; then
+									echo "This user does not exist!"
+								else
+									read -p "Enter new shell path: " NEWSHELL
+									usermod -s $NEWSHELL $USERNAME
+								fi
+								;;
+							5)
+								USERNAME=""
+								NEWCOMMENT=""
+								echo "************************************"					
+								echo "Change user comment"
+								echo "************************************"								
+
+								read -p "Enter username" USERNAME
+								id $USERNAME &> /dev/null
+								if [ $? = 1 ]; then
+									echo "This user does not exist!"
+								else
+									read -p "Enter new comment: " NEWCOMMENT
+									usermod -c "$NEWCOMMENT" $USERNAME
+								fi
+								;;
+							6)
+								USERNAME=""
+								echo "************************************"					
+								echo "Change user password"
+								echo "************************************"								
+
+								id $USERNAME &> /dev/null
+								if [ $? = 1 ]; then
+									echo "This user does not exist!"
+								else
+									NEWPASSWORD=""
+									read -p "Enter new password: " NEWPASSWORD
+									usermod -p $NEWPASSWORD $USERNAME
+								fi
+								;;
+							7)
+								ATTRIBUTES_FLAG=0
+								;;
+							*)
+								echo "Invalid input";;
+							esac
+						echo -e "\nPress any button to continue"
+						read -n 1
+					done					
+					;;
+				6)
+					echo "************************************"
+					echo "Returning to the main menu!"
+					echo "************************************"
+					sleep 2
+					return
+					;;
+				*)
+					echo "Invalid input";;
+			esac
+		done
+
 }
 
 networkInfo() {
